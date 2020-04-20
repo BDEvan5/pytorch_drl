47a8b268e4d46f23b24f4ebae3cbdbe8bd89e903
Benjy <19811799@sun.ac.za>
2020-04-20 15:33:33 -0200

    My own v1
    My neater version

--- My A2C Baseline modified.py
+++ My A2C Baseline modified.py
@@ -1,84 +1,39 @@
 import tensorflow as tf
 import numpy as np
 import gym 
-import tensorflow.keras.layers as kl
-import tensorflow.keras.losses as kls
-import tensorflow.keras.optimizers as ko
-
-# class ProbDist(tf.keras.Model):
-#     def call(self, logits, **kwargs):
-#         return tf.squeeze(tf.random.categorical(logits, 1), axis=-1)
-
-
-# class Model(tf.keras.Model):
-#     def __init__(self, num_actions):
-#         super().__init__('mlp_policy')
-
-#         self.hidden_values = tf.keras.layers.Dense(128, activation='relu')
-#         self.value = tf.keras.layers.Dense(1)
-
-#         self.hidden_logs = tf.keras.layers.Dense(128, activation='relu')
-#         self.logits = tf.keras.layers.Dense(num_actions)
-#         self.dist = ProbDist()
-
-#     def call(self, inputs, **kwargs):
-#         x = tf.convert_to_tensor(inputs)
-
-#         hidden_logs = self.hidden_logs(x)
-#         logits = self.logits(hidden_logs)
-
-#         hidden_values = self.hidden_values(x)
-#         value = self.value(hidden_values)
-
-#         return logits, value
-
-#     def action_value(self, obs):
-#         logits, value = self.predict_on_batch(obs)
-
-#         # converts logits to action.
-#         # action = tf.squeeze(tf.random.categorical(logits, 1), axis=-1)
-#         action = self.dist.predict_on_batch(logits)
-#         # he then squeezes both again, not sure why, possible to remove the slice.
-#         value = tf.squeeze(value, axis=-1)
-#         action = tf.squeeze(action, axis=-1)
-#         # print(action)
-
-#         return action, value
-
-class ProbabilityDistribution(tf.keras.Model):
-  def call(self, logits, **kwargs):
-    # Sample a random categorical action from the given logits.
-    return tf.squeeze(tf.random.categorical(logits, 1), axis=-1)
-
+from matplotlib import pyplot as plt
 
 
 class Model(tf.keras.Model):
-  def __init__(self, num_actions):
-    super().__init__('mlp_policy')
-    # Note: no tf.get_variable(), just simple Keras API!
-    self.hidden1 = kl.Dense(128, activation='relu') # these are separate networks I think, not sure why. 
-    self.hidden2 = kl.Dense(128, activation='relu')
-    self.value = kl.Dense(1, name='value')
-    # Logits are unnormalized log probabilities.
-    self.logits = kl.Dense(num_actions, name='policy_logits')
-    self.dist = ProbabilityDistribution()
+    def __init__(self, num_actions):
+        super().__init__('mlp_policy')
 
-  def call(self, inputs, **kwargs):
-    # Inputs is a numpy array, convert to a tensor.
-    x = tf.convert_to_tensor(inputs)
-    # Separate hidden layers from the same input tensor.
-    hidden_logs = self.hidden1(x)
-    hidden_vals = self.hidden2(x)
-    return self.logits(hidden_logs), self.value(hidden_vals)
+        self.hidden_values = tf.keras.layers.Dense(128, activation='relu')
+        self.value = tf.keras.layers.Dense(1)
 
-  def action_value(self, obs):
-    # Executes `call()` under the hood.
-    logits, value = self.predict_on_batch(obs)
-    action = self.dist.predict_on_batch(logits)
-    # Another way to sample actions:
-    #   action = tf.random.categorical(logits, 1)
-    # Will become clearer later why we don't use it.
-    return np.squeeze(action, axis=-1), np.squeeze(value, axis=-1)
+        self.hidden_logs = tf.keras.layers.Dense(128, activation='relu')
+        self.logits = tf.keras.layers.Dense(num_actions)
+
+    def call(self, inputs, **kwargs):
+        x = tf.convert_to_tensor(inputs)
+
+        hidden_logs = self.hidden_logs(x)
+        logits = self.logits(hidden_logs)
+
+        hidden_values = self.hidden_values(x)
+        value = self.value(hidden_values)
+
+        return logits, value
+
+    def action_value(self, obs):
+        logits, value = self.predict_on_batch(obs)
+
+        action = tf.squeeze(tf.random.categorical(logits, 1), axis=-1)
+
+        value = tf.squeeze(value, axis=-1)
+        action = tf.squeeze(action, axis=-1)
+
+        return action.numpy(), value
 
 
 
@@ -88,7 +43,51 @@ def _value_loss(returns, values):
     loss = value_c * tf.keras.losses.mean_squared_error(returns, values)
     return loss
 
+def _logits_loss(actions_and_advs, logits):
+        entropy_c = 1e-4
+        print("loss acts advs")
+        print(actions_and_advs)
+        # print(actions_and_advs.numpy())
+        acts, advs = tf.split(actions_and_advs, 2, axis=-1)
 
+        weighted_ce = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+        acts = tf.cast(acts, tf.int32)
+        # I think the sample weigths scale the loss according to how big the adv is.
+        # policy loss is the entropy scaled to adv. 
+        # entropy is how different was the choice from what was expected. 
+        policy_loss = weighted_ce(acts, logits, sample_weight=advs)
+
+        probs = tf.nn.softmax(logits)
+        entropy_loss = tf.keras.losses.categorical_crossentropy(probs, probs)
+
+        total_policy_loss = policy_loss - entropy_c * entropy_loss
+        return total_policy_loss
+
+
+def plot(values, moving_avg_period, title, figure_n):
+    plt.figure(figure_n)
+    plt.clf()        
+    plt.title(title)
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(values)
+
+    moving_avg = get_moving_average(moving_avg_period, values)
+    plt.plot(moving_avg)    
+    moving_avg = get_moving_average(moving_avg_period * 5, values)
+    plt.plot(moving_avg)    
+    plt.pause(0.001)
+
+def get_moving_average(period, values):
+
+    moving_avg = np.zeros_like(values)
+
+    for i, avg in enumerate(moving_avg):
+        if i > period:
+            moving_avg[i] = np.mean(values[i-period:i])
+        # else already zero
+    return moving_avg
 
 
 class ReplayMem:
@@ -138,7 +137,7 @@ class Agent:
         self.buffer = buffer
 
         self.model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=lr),
-                    loss=[self._logits_loss, _value_loss])
+                    loss=[_logits_loss, _value_loss])
 
         self.model.action_value(env.reset()[None, :])
         self.model.summary()
@@ -163,7 +162,7 @@ class Agent:
                     print("Episode: %03d, Reward: %03d" % (len(ep_rewards) - 1, ep_rewards[-2]))
 
             self._update_network(next_obs)
-            #TODO: add plot
+            plot(ep_rewards, 10, "CartpoleTraining", 2)
 
         return ep_rewards
             
@@ -201,26 +200,8 @@ class Agent:
                 env.render()
         return ep_reward
 
-    def _logits_loss(self, actions_and_advs, logits):
-        entropy_c = 1e-4
-        print("loss acts advs")
-        print(actions_and_advs)
-        # print(actions_and_advs.numpy())
-        acts, advs = tf.split(actions_and_advs, 2, axis=-1)
 
-        weighted_ce = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-        acts = tf.cast(acts, tf.int32)
-        # I think the sample weigths scale the loss according to how big the adv is.
-        # policy loss is the entropy scaled to adv. 
-        # entropy is how different was the choice from what was expected. 
-        policy_loss = weighted_ce(acts, logits, sample_weight=advs)
-
-        probs = tf.nn.softmax(logits)
-        entropy_loss = tf.keras.losses.categorical_crossentropy(probs, probs)
-
-        total_policy_loss = policy_loss - entropy_c * entropy_loss
-        return total_policy_loss
 
 
 if __name__ == "__main__":
