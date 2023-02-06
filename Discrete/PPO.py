98eab49b20d7f2cd6f028bb8e6b2b129242e86f2
Benjamin <benjaminevans316@gmail.com>
2023-02-06 11:33:55 +0200

    Clean PPO algorithm

--- Discrete/PPO.py
+++ Discrete/PPO.py
@@ -1,4 +1,3 @@
-
 import gym
 import torch
 import torch.nn as nn
@@ -14,12 +13,11 @@ eps_clip      = 0.1
 K_epoch       = 3
 T_horizon     = 100
 
-class Network(nn.Module):
+class Actor(nn.Module):
     def __init__(self, obs_space, action_space, h_size):
-        super(Network, self).__init__()
+        super(Actor, self).__init__()
         self.fc1 = nn.Linear(obs_space, h_size)
         self.fc_pi = nn.Linear(h_size, action_space)
-        self.fc_v  = nn.Linear(h_size,1)
 
     def pi(self, x, softmax_dim = 0):
         x = F.relu(self.fc1(x))
@@ -27,6 +25,12 @@ class Network(nn.Module):
         prob = F.softmax(x, dim=softmax_dim)
         return prob
     
+class Critic(nn.Module):
+    def __init__(self, obs_space, action_space, h_size):
+        super(Critic, self).__init__()
+        self.fc1 = nn.Linear(obs_space, h_size)
+        self.fc_v  = nn.Linear(h_size,1)
+
     def v(self, x):
         x = F.relu(self.fc1(x))
         v = self.fc_v(x)
@@ -43,40 +47,18 @@ class PPO:
         self.optimizer = None
 
     def create_agent(self, h_size):
-        self.network = Network(self.obs_space, self.action_space, h_size)
-        self.optimizer = optim.Adam(self.network.parameters(), lr=learning_rate)
-      
-    def put_data(self, transition):
-
-        self.data.append(transition)
+        self.actor = Actor(self.obs_space, self.action_space, h_size)
+        self.critic = Critic(self.obs_space, self.action_space, h_size)
+        self.optimizer = optim.Adam(list(self.actor.parameters()) + list(self.critic.parameters()), lr=learning_rate)
         
     def put_action_data(self, s, a, s_prime, r, done):
-        prob = self.network.pi(torch.from_numpy(s).float())
+        prob = self.actor.pi(torch.from_numpy(s).float())
         transition = (s, a, r, s_prime, prob[a].item(), done)
         
         self.data.append(transition)
         
-    def make_batch(self):
-        s_lst, a_lst, r_lst, s_prime_lst, prob_a_lst, done_lst = [], [], [], [], [], []
-        for transition in self.data:
-            s, a, r, s_prime, prob_a, done = transition
-            
-            s_lst.append(s)
-            a_lst.append([a])
-            r_lst.append([r])
-            s_prime_lst.append(s_prime)
-            prob_a_lst.append([prob_a])
-            done_mask = 0 if done else 1
-            done_lst.append([done_mask])
-            
-        s,a,r,s_prime,done_mask, prob_a = torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), \
-                                          torch.tensor(r_lst), torch.tensor(s_prime_lst, dtype=torch.float), \
-                                          torch.tensor(done_lst, dtype=torch.float), torch.tensor(prob_a_lst)
-        self.data = []
-        return s, a, r, s_prime, done_mask, prob_a
-        
     def act(self, obs):
-        prob = self.network.pi(torch.from_numpy(obs).float())
+        prob = self.actor.pi(torch.from_numpy(obs).float())
         m = Categorical(prob)
         a = m.sample().item()
 
@@ -86,11 +68,12 @@ class PPO:
         if len(self.data) < T_horizon:
             return
 
-        s, a, r, s_prime, done_mask, prob_a = self.make_batch()
+        s, a, r, s_prime, done_mask, prob_a = make_data_batch(self.data)
+        self.data = []
 
         for i in range(K_epoch):
-            td_target = r + gamma * self.network.v(s_prime) * done_mask
-            delta = td_target - self.network.v(s)
+            td_target = r + gamma * self.critic.v(s_prime) * done_mask
+            delta = td_target - self.critic.v(s)
             delta = delta.detach().numpy()
 
             advantage_lst = []
@@ -101,13 +84,13 @@ class PPO:
             advantage_lst.reverse()
             advantage = torch.tensor(advantage_lst, dtype=torch.float)
 
-            pi = self.network.pi(s, softmax_dim=1)
+            pi = self.actor.pi(s, softmax_dim=1)
             pi_a = pi.gather(1,a)
             ratio = torch.exp(torch.log(pi_a) - torch.log(prob_a))  # a/b == exp(log(a)-log(b))
 
             surr1 = ratio * advantage
             surr2 = torch.clamp(ratio, 1-eps_clip, 1+eps_clip) * advantage
-            loss = -torch.min(surr1, surr2) + F.smooth_l1_loss(self.network.v(s) , td_target.detach())
+            loss = -torch.min(surr1, surr2) + F.smooth_l1_loss(self.critic.v(s) , td_target.detach())
 
             self.optimizer.zero_grad()
             loss.mean().backward()
@@ -125,13 +108,29 @@ class PPO:
 
         torch.save(self.network, '%s/%s_network.pth' % (directory, filename))
 
-        # print(f"Agent Saved: {filename}")
+def make_data_batch(data):
+    s_lst, a_lst, r_lst, s_prime_lst, prob_a_lst, done_lst = [], [], [], [], [], []
+    for transition in data:
+        s, a, r, s_prime, prob_a, done = transition
+        
+        s_lst.append(s)
+        a_lst.append([a])
+        r_lst.append([r])
+        s_prime_lst.append(s_prime)
+        prob_a_lst.append([prob_a])
+        done_mask = 0 if done else 1
+        done_lst.append([done_mask])
+        
+    s,a,r,s_prime,done_mask, prob_a = torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), \
+                                        torch.tensor(r_lst), torch.tensor(s_prime_lst, dtype=torch.float), \
+                                        torch.tensor(done_lst, dtype=torch.float), torch.tensor(prob_a_lst)
+                                        
+    return s, a, r, s_prime, done_mask, prob_a
 
 def main():
     env = gym.make('CartPole-v1')
     model = PPO(4, 2, "myfriend")
     model.create_agent(100)
-    # model.create_agent(256)
     score = 0.0
     print_interval = 20
 
